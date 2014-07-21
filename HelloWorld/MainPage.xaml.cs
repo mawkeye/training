ec77cc6575ba3ea0c2e5db9f7e6ee82c01da1d6e
Mart Walt <mawkeye@gmail.com>
2014-07-21 11:50:51 +0200

    Signed-off-by: Mart Walt <mawkeye@gmail.com>

---
+++ HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=391641 dokumentiert.
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet werden kann oder auf die innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Wird aufgerufen, wenn diese Seite in einem Rahmen angezeigt werden soll.
+        /// </summary>
+        /// <param name="e">Ereignisdaten, die beschreiben, wie diese Seite erreicht wurde.
+        /// Dieser Parameter wird normalerweise zum Konfigurieren der Seite verwendet.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Button myButton = new Button();
+            myButton.Name = "clickMeButton";
+            myButton.Content = "Click Me";
+            myButton.Width = 200;
+            myButton.Height = 100;
+            myButton.Margin = new Thickness(46, 102, 0, 0);
+            myButton.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Left;
+            myButton.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Top;
+            myButton.Background = new SolidColorBrush(Colors.Aqua);
+            myButton.Click += clickMeButton_Click;
+
+            MyLayoutGrid.Children.Add(myButton);
+        }
+
+        private void clickMeButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBlock.Text = "Hello World";
+        }
+    }
+}
