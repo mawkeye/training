07a617161401ff209e1182e0428c99ae08f6951e
mawkeye <mawkeye@gmail.com>
2014-07-24 19:47:44 +0200

    Lesson 09

---
+++ NavigationExample/Page3.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkID=390556 dokumentiert.
+
+namespace NavigationExample
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet werden kann oder auf die innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class Page3 : Page
+    {
+        public Page3()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Wird aufgerufen, wenn diese Seite in einem Frame angezeigt werden soll.
+        /// </summary>
+        /// <param name="e">Ereignisdaten, die beschreiben, wie diese Seite erreicht wurde.
+        /// Dieser Parameter wird normalerweise zum Konfigurieren der Seite verwendet.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var mypages = Frame.BackStack.Aggregate("", (current, page) => current + (page.SourcePageType.ToString() + "\n"));
+
+            PagesTextBlock.Text = mypages;
+
+            Frame.BackStack.RemoveAt(Frame.BackStackDepth -1);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+                Frame.GoBack();
+            //throw new NotImplementedException();
+        }
+    }
+}
